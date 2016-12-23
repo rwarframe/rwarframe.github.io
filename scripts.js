38014b5e4234c0dc3e524f53089838ee90202b9e
Khuon23 <scflyboy23@aol.com>
2016-12-23 16:41:46 -0600

    Message change

--- scripts.js
+++ scripts.js
@@ -104,7 +104,7 @@ flair.sendChoice = function() {
         }
     }
     
-    window.open('http://www.reddit.com/message/compose/?to=PokemonFlairBot&subject='+
+    window.open('http://www.reddit.com/message/compose/?to=WarframeFlairBot&subject='+
         flair.current_choice+
         '&message='+flair_text+'%0A'+
         subreddits)
